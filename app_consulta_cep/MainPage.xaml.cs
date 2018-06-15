436a6594f95df60a31e855214dcf9e59279eb676
lvlima <luizvictor.al@gmail.com>
2018-06-15 18:01:06 -0300

    Add files via upload

---
+++ app_consulta_cep/MainPage.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using app_consulta_cep.Servico.Modelo;
+using app_consulta_cep.Servico;
+
+namespace app_consulta_cep
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            btnBuscar.Clicked += BuscarCep;
+
+        }
+
+        private void BuscarCep(Object sender, EventArgs args)
+        {
+            string cep = inputCEP.Text.Trim();
+
+            if (isValidCEP(cep))
+            {
+                try
+                {
+                    Endereco endereco = ViaCEPServico.BuscarEnderecoViaCEP(cep);
+
+                    if (endereco !=null)
+                    {
+                        txtCEP.Text = string.Format("Endereço: {0} {1}, {2} - {3}/{4} - {5}", endereco.logradouro, endereco.complemento, endereco.bairro, endereco.localidade, endereco.uf, endereco.cep);    
+                    }
+                    else
+                    {
+                        txtCEP.Text = "Não foi encontrado um endereço para o CEP informado.";
+                    }
+
+
+
+                } catch(Exception e)
+                {
+                    DisplayAlert("Erro", "Erro na conexão com o webservice. " + e.Message + ".", "OK");    
+                }
+            }
+
+        }
+
+        private bool isValidCEP(string cep)
+        {
+            if (cep.Length != 8)
+            {
+                DisplayAlert("Erro", "O CEP deve possuir 8 caracteres.", "OK");
+                return false;    
+            }
+
+            int novoCEP = 0;
+            if (!int.TryParse(cep, out novoCEP))
+            {
+                DisplayAlert("Erro", "O CEP deve possuir apenas caracteres numéricos.", "OK");
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ app_consulta_cep/Servico/ViaCEPServico.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net;
+using app_consulta_cep.Servico.Modelo;
+using Newtonsoft.Json;
+
+namespace app_consulta_cep.Servico
+{
+    public class ViaCEPServico
+    {
+        private static string EnderecoURL = "https://viacep.com.br/ws/{0}/json/";
+
+        public static Endereco BuscarEnderecoViaCEP(string cep)
+        {
+            string NovoEnderecoURL = string.Format(EnderecoURL, cep);
+
+            WebClient wc = new WebClient();
+            string Conteudo = wc.DownloadString(NovoEnderecoURL);
+
+            Endereco end = JsonConvert.DeserializeObject<Endereco>(Conteudo);
+
+            if (end.cep == null)
+            {
+                return null;
+            }
+
+            return end;
+        }
+
+    }
+}
